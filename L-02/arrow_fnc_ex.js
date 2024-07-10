a943488bf12fb523e03ac197aaecdffdc599a16c
ThitiwutBanK <s6506022620087@email.kmutnb.ac.th>
2024-07-10 11:48:36 +0700

    upupup

---
+++ L-02/arrow_fnc_ex.js
@@ -0,0 +1,21 @@
+const sum = (firstParam, secondParam) => {
+  return firstParam + secondParam;
+};
+console.log(sum(2, 5)); // Prints: 7
+
+const printHello = () => {
+    console.log("hello");
+    };
+printHello(); // Prints: hello
+
+
+
+
+const checkWeight = weight => {
+    console.log(`Baggage weight : ${weight} kilograms.`);
+  };
+checkWeight(25); // Prints: Baggage weight : 25 kilograms.
+
+
+const multiply = (a, b) => a * b;
+console.log(multiply(2, 30)); // Prints: 60
